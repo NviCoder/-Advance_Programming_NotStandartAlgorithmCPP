9afe2b6745582ab0a03817f19cfccfc8c5589e62
NviCoder <44750316+NviCoder@users.noreply.github.com>
2020-05-06 12:55:16 +0300

    Update algo.h

--- alg.h
+++ alg.h
@@ -2,80 +2,93 @@
 #include <algorithm>
 #include <iterator>
 #include <iostream>
-using std::cout; using std::endl;
+using std::cout;
+using std::endl;
 
+/*
+  This Algorithm swap sum up n elements on this container and puts this in the beginig of the continaer.
+  p = Add_n(b,e,n) ---> begain , end , number.
+  Returns
+  if length/n = 0 -> e
+  Else -> 1....n-1
+*/
 template <typename T>
-const T Median(const T& b_it, const T& e_it, unsigned int n)
+const T Add_n(const T &b_it, const T &e_it, unsigned int n)
 {
-	unsigned int length = std::distance(b_it,e_it);
-	T inserter_it = e_it;
+	unsigned int length = std::distance(b_it, e_it);
+	T inserter_it = b_it;
 
 	if (length % n != 0 || length == 0)
-		return inserter_it;
-	
-	std::sort(b_it, e_it);
-
-	if (n == 1)
-		return inserter_it;
-
-	T m_it = b_it;
-	int m_it_position = 0;
-	bool first_iteration = true;
-	inserter_it = b_it;
-
-	while (m_it_position + n <= length)
 	{
-		if (first_iteration)
-		{
-			m_it = b_it + (n / 2);
-			m_it_position += (n / 2);
-			first_iteration = false;
-		}
-		else
-		{
-			m_it += n;
-			m_it_position += n;
-		}
-
-		if ((n % 2) != 0) //n is odd & bigger then 1 - median is the middle value
-			*inserter_it = *m_it;
-		else //n is even - median is the avg of the 2 middle values
-			*inserter_it = (*m_it + *(m_it - 1)) / 2;
-
-		inserter_it++;
+		int sum = std::accumulate(b_it, e_it, 0);
+		*(inserter_it) = sum;
 	}
 
+	else
+	{
+		if (n == 1)
+			return inserter_it;
+
+		T currunt_it_position = b_it;
+
+		while (currunt_it_position + n <= length)
+		{
+			T end_n = currunt_it_position + n;
+			int sum = std::accumulate(currunt_it_position, end_n, 0);
+			*(inserter_it) = sum;
+			inserter_it++;
+			currunt_it_position = end_n;
+		}
+	}
 	return inserter_it;
 };
 
+//----------------------------------------------------------------//
+
+/*
+  This Algorithm swap between two Adjacent elements on this container
+  P = Transpose(b,e) ---> begain , end
+  Returns
+  Even - e
+  Odd - e - 1
+ */
 template <typename T>
-const T Transpose(const T& b_it, const T& e_it)
+const T Transpose(const T &b_it, const T &e_it)
 {
-	int length = std::distance(b_it,e_it);
+	int length = std::distance(b_it, e_it);
 	T it = b_it;
-	for (int i = 0; i < length-1; i += 2)
-		std::iter_swap(it + i, it + (i + 1));
-	if (length % 2 == 0)
+	for (int i = 0; i < length - 1; i += 2)
+		std::iter_swap(it + i, it + (i + 1)); //Swapping the elements
+	if (length % 2 == 0)					  // Checks if the length is even or odd!
 		return e_it;
 	else
 		return (e_it - 1);
 };
 
-template <typename T, typename F>
-const T Transformations(const T& b_it, const T& e_it, F&& f)
+/*
+ This Algorithm invoke function "f" on two Adjacent elements on the container and so on.
+  p = Transform(b,e,o,f) ---> begain , end , other begain , function.
+  Returns
+  Even - The place after the last element on the second container
+  Odd - o.
+
+*/
+template <typename T, typename G, typename F>
+const G Transform(const T &b_it, const T &e_it, const G &o_it, F &&f)
 {
 	int length = std::distance(b_it, e_it);
 	if (length % 2 != 0 || length == 0)
-		return e_it;
+		return o_it;
 	else
 	{
-		T insertion_it = b_it;
+		G insertion_it = o_it;
 
 		int n = length / 2;
 		for (int i = 0; i < n; i++)
 		{
 			*(insertion_it) = f(b_it[2 * i], b_it[(2 * i) + 1]);
-			insertion_it++;;
+			insertion_it++;
+			;
 		}
 
 		return insertion_it;
